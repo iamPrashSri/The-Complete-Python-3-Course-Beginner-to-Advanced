712b6a5e412be9b2e547e439c5e78cf862d2b4a8
Prashant Srivastava <56686827+iamPrashSri@users.noreply.github.com>
2020-03-02 13:45:16 +0530

    Add files via upload

---
+++ AdvancedPythonConcepts/PepGuidelines.py
@@ -0,0 +1,19 @@
+# 1. Import on separate lines
+# 2. from myModule import foo, bar, foobar
+import os
+import sys
+
+
+# Defining Classes and Functions
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def secondFunction():
+    print("Second Function")
+
+my_list = [1, 3,
+           4, 5,
+           6, 7]
